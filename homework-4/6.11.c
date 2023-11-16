7e45e09cdeb6669928d1fb28221bb1deeedaef29
112360216 <t112360216@ntut.org.tw>
2023-11-16 18:42:19 +0800

    gaga1116

---
+++ homework-4/6.11.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+void swap(int *a,int *b){
+    int tmp = (*a);
+    (*a) = (*b);
+    (*b) = tmp;
+}
+
+
+int main(){
+    srand(time(0)<<5);
+    int len = rand()%100+10;
+    int arr[len];
+    printf("Original value is :\n");
+    for(int i=0;i<len;i++){
+        i[arr] = rand() % INT_MAX;
+        printf("%d ",i[arr]);
+    }
+    putchar('\n');
+    printf("New value is :\n");
+    for(int i=0;i<len;i++){
+        for(int j=0;j<len-i-1;j++){
+            if(j[arr] > (j+1)[arr]){
+                swap(arr+j,arr+j+1);
+            }
+        }
+    }
+    for(int i=0;i<len;i++) printf("%d ",i[arr]);
+}
